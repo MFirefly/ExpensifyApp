8d331d717c9166b89abd89f24c57e873de7c51fa
MFirefly <maja.filakovic@gmail.com>
2018-07-20 10:20:36 +0200

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -4,32 +4,56 @@
 // __dirname - contains path to the current location
 
 const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    mode: 'development',
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/, //Only files ending with .js
-            exclude: /node_modules/
-        }, {
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ],
-            test: /\.s?css$/
-        }]
-    },
-    devtool: "cheap-module-eval-source-map",
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
-    }
+//Advantage of exporting function instead of object is that
+//function is then called with some arguments and with these
+//arguments we can check if the build is for development or
+//for production. The argument value is given upon running
+//the "webpack -p" command
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    const CSSExtract = new ExtractTextPlugin("styles.css");
+
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        mode: 'development',
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/, //Only files ending with .js
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,
+                use: CSSExtract.extract({
+                    use: [
+                        {
+                            loader: "css-loader",
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: "sass-loader",
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                })
+            }]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProduction ? "source-map" : "inline-source-map",
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        }
+    };
 };
-
